f59bfd7498f2e76c706dcbf6b5b7f16375a393a4
文煌 <wwh1004@hotmail.com>
2019-07-28 13:08:30 +0800

    Bump version

--- Tool.Loader.Auto/Properties/AssemblyInfo.cs
+++ Tool.Loader.Auto/Properties/AssemblyInfo.cs
@@ -2,6 +2,3 @@ using System.Reflection;
 
 [assembly: AssemblyTitle("Tool.Loader.Auto")]
 [assembly: AssemblyProduct("Tool.Loader.Auto")]
-[assembly: AssemblyCopyright("Copyright © 2019 Wwh")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
